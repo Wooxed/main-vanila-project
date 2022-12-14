3724b4e35c6f4a09c14687e331efbac217aad4df
Sergei <samkorbut@gmail.com>
2022-12-14 17:36:37 +0400

    Unit tests, 03 lesson - Functions

---
+++ src/03/03.ts
@@ -0,0 +1,50 @@
+import {StudentType} from "../02/02";
+import {CityType, GovernmentBuildingType, HouseType} from "../02/02_02";
+
+
+export const sum = (a: number, b: number) => {
+    return a + b
+}
+console.log(sum)
+
+
+export function sum2(a: number, b: number) {
+    return a + b
+}
+
+console.log(sum2)
+export function addSkill(st: StudentType, skill: string) {
+    st.technologies.push({
+        id: new Date().getTime(),
+        title: skill
+    })
+}
+
+export function makeStudentActive(s: StudentType) {
+    s.isActive = true
+}
+
+export const doesStudentLiveIn = (s: StudentType, cityName: string) => {
+    return s.address.city.title === cityName
+}
+
+export const addMoneyToBudget = (building: GovernmentBuildingType, budget: number)=> {
+building.budget += budget;
+}
+
+export const repairHouse = (houseType: HouseType)=>{
+    houseType.repaired = true
+}
+
+export const toFireStaff = (building: GovernmentBuildingType, staffCountToFire: number)  => {
+building.staffCount -= staffCountToFire
+}
+
+export const toHireStaff = (building: GovernmentBuildingType, staffCountToHire: number)  => {
+    building.staffCount += staffCountToHire
+}
+
+export  const createMessage = (props: CityType)=> {
+    // return 'Hello ' + props.title + ' citizens. I want you be happy. All ' + props.citizensNumber + ' men'
+    return `Hello ${props.title} citizens. I want you be happy. All ${props.citizensNumber} men`
+}
